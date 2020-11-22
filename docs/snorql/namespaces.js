e5398defff339e5515a6c2a58924c80bfe6cb825
nakamura196 <na.kamura.1263@gmail.com>
2020-11-23 06:21:25 +0900

    initial commit

---
+++ docs/snorql/namespaces.js
@@ -0,0 +1,28 @@
+var D2R_namespacePrefixes = {
+    "jps": "https://jpsearch.go.jp/term/property#",
+    "schema": "http://schema.org/",
+    "type": "https://nakamura196.github.io/hi_person/term/type/",
+    "chname": "https://nakamura196.github.io/hi_person/entity/chname/",
+    "place": "https://nakamura196.github.io/hi_person/entity/place/",
+    "time": "https://nakamura196.github.io/hi_person/entity/time/",
+    //"work": "https://jpsearch.go.jp/entity/work/",
+    //"role": "https://jpsearch.go.jp/term/role/",
+    //"keyword": "https://jpsearch.go.jp/term/keyword/",
+    "skos": "http://www.w3.org/2004/02/skos/core#",
+    //"gvname": "https://jpsearch.go.jp/entity/gvname/",
+    //"ncname": "https://jpsearch.go.jp/entity/ncname/",
+    //"ncplace": "https://jpsearch.go.jp/entity/ncplace/",
+    //"series": "https://jpsearch.go.jp/entity/series/",
+    "xsd": "http://www.w3.org/2001/XMLSchema#",
+    rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+    owl: 'http://www.w3.org/2002/07/owl#',
+    dct: "http://purl.org/dc/terms/",
+    genji: "https://w3id.org/kouigenjimonogatari/api/property/",
+    "prop-ja": "http://ja.dbpedia.org/property/",
+    "emperor": "https://nakamura196.github.io/hi_person/entity/emperor/",
+    "kani": "https://nakamura196.github.io/hi_person/term/kani/",
+    "role": "https://nakamura196.github.io/hi_person/term/role/",
+    "keyword": "https://nakamura196.github.io/hi_person/term/keyword/"
+};
+
